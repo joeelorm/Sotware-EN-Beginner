74e49a7e5f10c4060db83cc5b5373f89480e5f74
joeelorm <joeelorm@outlook.com>
2021-01-16 18:22:56 +0100

    Create kelvin_weather.js

---
+++ JavaScript Syntax I/kelvin_weather.js
@@ -0,0 +1,19 @@
+// Kelvin Weather
+// Deep in his mountain-side meteorology lab, the mad scientist Kelvin has mastered weather prediction.
+
+// Recently, Kelvin began publishing his weather forecasts on his website. However there’s a problem: All of his forecasts describe the temperature in Kelvin.
+
+// With our knowledge of JavaScript, let’s convert Kelvin to Celsius, then to Fahrenheit.
+
+const kelvin = 0; // Constant variable called kelvin
+
+celsius = kelvin - 273;
+
+let fahrenheit = celsius * (9/5) + 32;
+
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
+
+let newton = celsius * (33/100);
+newton = Math.floor(newton)
+console.log(`The temperature is ${newton} Newton.`)
